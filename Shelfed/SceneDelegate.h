0cfd6cd464a9c698d15b95669948d5454c8537f1
Clara Kim <claraj@fb.com>
2020-07-13 15:19:24 -0400

    Initial Commit

---
+++ Shelfed/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Shelfed
+//
+//  Created by Clara Kim on 7/13/20.
+//  Copyright © 2020 Clara Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ Shelfed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Shelfed
+//
+//  Created by Clara Kim on 7/13/20.
+//  Copyright © 2020 Clara Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
