35f494a07b00e4c46b0321b4368624cf7cab69c6
Clara Kim <claraj@fb.com>
2020-07-15 18:45:45 -0400

    Persisting user

---
+++ Shelfed/View Controllers/FavoritesViewController.m
@@ -0,0 +1,32 @@
+//
+//  FavoritesViewController.m
+//  Shelfed
+//
+//  Created by Clara Kim on 7/15/20.
+//  Copyright © 2020 Clara Kim. All rights reserved.
+//
+
+#import "FavoritesViewController.h"
+
+@interface FavoritesViewController ()
+
+@end
+
+@implementation FavoritesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
--- Shelfed/View Controllers/HomeViewController.m
+++ Shelfed/View Controllers/HomeViewController.m
@@ -33,9 +33,16 @@ GoogleBooksAPIManager *manager;
     
     manager = [GoogleBooksAPIManager new];
     
+    __weak typeof(self) weakSelf = self;
     [manager defaultHomeQuery:^(NSArray * _Nonnull books, NSError * _Nonnull error) {
-        self.books = books;
-        [self.tableView reloadData];
+        __strong typeof(self) strongSelf = weakSelf;
+        if(error!=nil){
+            NSLog(@"Error getting default feed!");
+        }
+        else{
+            strongSelf.books = books;
+            [strongSelf.tableView reloadData];
+        }
     }];
     
 }
@@ -50,29 +57,56 @@ GoogleBooksAPIManager *manager;
     return cell;
 }
 #pragma mark - UISearchBarDelegate
+- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar{
+    self.searchBar.showsCancelButton = YES;
+}
 - (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText{
 
     NSString *searchableText = searchText;
     searchableText = [[searchableText componentsSeparatedByCharactersInSet:[NSCharacterSet punctuationCharacterSet]] componentsJoinedByString:@""];
     searchableText = [[searchableText componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] componentsJoinedByString:@"+"];
     
+    __weak typeof(self) weakSelf = self;
     if(searchableText.length != 0){
         [manager searchBooks:searchableText andCompletion:^(NSArray * _Nonnull books, NSError * _Nonnull error) {
+            __strong typeof(self) strongSelf = weakSelf;
             if(error!=nil){
                 NSLog(@"Error searching!");
             }
             else{
-                self.books = books;
-                [self.tableView reloadData];
+                strongSelf.books = books;
+                [strongSelf.tableView reloadData];
             }
         }];
     }
     else{
-        [manager defaultHomeQuery:^(NSArray * _Nonnull books, NSError * _Nonnull error) {
-            self.books = books;
-            [self.tableView reloadData];
+        [manager defaultHomeQuery:^(NSArray * _Nonnull books, NSError * _Nonnull error){
+            __strong typeof(self) strongSelf = weakSelf;
+            if(error!=nil){
+                NSLog(@"Error getting default feed!");
+            }
+            else{
+                strongSelf.books = books;
+                [strongSelf.tableView reloadData];
+            }
         }];
     }
+}
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar{
+    self.searchBar.showsCancelButton = NO;
+    self.searchBar.text = @"";
+    [self.searchBar resignFirstResponder];
+    __weak typeof (self) weakSelf = self;
+    [manager defaultHomeQuery:^(NSArray * _Nonnull books, NSError * _Nonnull error) {
+        __strong typeof(self) strongSelf = weakSelf;
+        if(error!=nil){
+            NSLog(@"Error getting default feed!");
+        }
+        else{
+            strongSelf.books = books;
+            [strongSelf.tableView reloadData];
+        }
+    }];
 }
 
 #pragma mark - Navigation
